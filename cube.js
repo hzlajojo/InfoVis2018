f39d354ab120bbac47cc9d2704a14fd6dce16a1f
hzlajojo <30767295+hzlajojo@users.noreply.github.com>
2018-06-13 00:34:24 +0900

    Add files via upload

---
+++ cube.js
@@ -0,0 +1,39 @@
+function cube()
+{
+	var width = window.innerWidth *7/15;
+  var height = window.innerHeight *7/15;
+	var camera, scene, renderer;
+	var mesh;
+	init();
+	animate();
+	function init() {
+		camera = new THREE.PerspectiveCamera( 70, width / height, 1, 1000 );
+		camera.position.z = 400;
+		scene = new THREE.Scene();
+		var texture = new THREE.TextureLoader().load( 'marvel.jpg' );
+		var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+		var material = new THREE.MeshBasicMaterial( { map: texture } );
+		mesh = new THREE.Mesh( geometry, material );
+		scene.add( mesh );
+		renderer = new THREE.WebGLRenderer( { antialias: true } );
+		renderer.setClearColor(obj.color , 1.0);// Color of renderer
+		renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( width, height );
+		document.body.appendChild( renderer.domElement );
+		//
+		window.addEventListener( 'resize', onWindowResize, false );
+	}
+	function onWindowResize() {
+		camera.aspect = width / height;
+		camera.updateProjectionMatrix();
+		renderer.setSize( width , height );
+	}
+	function animate() {
+		requestAnimationFrame( animate );
+		renderer.setClearColor(obj.color,1.0);// Color of renderer
+		mesh.rotation.x += obj.Rotation_X;
+		mesh.rotation.y += obj.Rotation_Y;
+		mesh.rotation.z += obj.Rotation_Z;
+		renderer.render( scene, camera );
+	}
+}
